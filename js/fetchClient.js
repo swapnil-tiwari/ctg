ca16e4bae0390bff7e81a5d6151ada34dc334137
Swapnil Tiwari <36299079+swapnil-tiwari@users.noreply.github.com>
2018-09-28 00:30:25 +0530

    UI Update+Bug Fixes

--- js/fetchClient.js
+++ js/fetchClient.js
@@ -2,7 +2,8 @@ async function func()
 {
 
  await setAPI();
- document.getElementById('startup_cap_dash').innerHTML=(await api.userdata(['username'])).data.username;
+ document.getElementById('startup_cap_dash_1').innerHTML=(await api.userdata(['username'])).data.username;
+ document.getElementById('startup_cap_dash_2').innerHTML=(await api.userdata(['username'])).data.username;
  document.getElementById('startup_cap_post').innerHTML=(await api.userdata(['username'])).data.username;
 
 }func();
